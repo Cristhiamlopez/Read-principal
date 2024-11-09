be2bb7b9e7f120027f4bd605fc0870ee3791b477
Cristhiam Lopez <cristhiamcamilolopez@gmail.com>
2024-11-09 16:02:28 -0500

    conexion b y f

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const conexion = mysql.createConnection({
     host: process.env.MYSQL_ADDON_HOST,
     database : process.env.MYSQL_ADDON_DB,
     user: process.env.MYSQL_ADDON_USER,
-    password: process.env.MYSQL_ADDON_PASSWORD
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    port:process.evn.MYSQL_ADDON_PORT
 
 });
 
